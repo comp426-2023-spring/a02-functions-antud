4dfee642f3e6d0c0f6c350b9eb8f170be74747e1
antud <atudela@unc.edu>
2023-01-24 22:51:54 -0500

    added days conditions

--- cli.js
+++ cli.js
@@ -4,10 +4,10 @@ import moment from 'moment-timezone';
 import fetch from 'node-fetch';
 import minimist from 'minimist';
 
-var argv = process.argv.slice(2); //taken directly from minimist site, but removed require at advice from logs
+var args = process.argv.slice(2); //taken directly from minimist site, but removed require at advice from logs
 
 // need to add the help text
-if (argv.h) {
+if (args.h) {
 	console.log('Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE');
 	console.log('-h            Show this help message and exit.');
 	console.log('-n, -s        Latitude: N positive; S negative.');
@@ -27,8 +27,23 @@ let lon = 0;
 //var maxTemp;
 //var minTemp;
 
-
+// Make a request
 const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + lat
 							+ '&longitude=' + lon
 							+ '&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_hours&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch'
 							+ '&timezone=' + timezone);  //America%2FNew_York')
+
+// Get the data from the request
+const data = await response.json();		
+
+//Create the response text using conditional statements
+const days = args.d;
+if (days == 0) {
+	console.log('today.');
+}
+else if (days > 1) {
+	console.log('in ' + days + ' days.');
+}
+else {
+	console.log('tomorrow');
+}
